50d612e5f8b01a03dee2b59291187bad5c2e93ed
fawwazhaiqal <fawwazfuaad21@gmail.com>
2024-02-07 22:11:38 +0800

    ep 7/2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,60 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+<html lang="en">
+<head>
+ <title>Demo</title>   
+</head>
+<body>
+    
+
+    <?php 
+        $books = [
+            [
+                'name' =>'Do Andorids Dream Electric',
+                'author' => 'Pilip',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ] ,
+            [
+                'name' =>'Do Andorids Dream Electric',
+                'author' => 'Andy Weir',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ] ,
+            [
+                'name' =>'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ] ,
+        ];
+
+        function filterByAuthor($book){
+            $filteredBooks =[];
+
+             foreach ($books as $book) {}
+                if ($book['author'] ==='Andy Weir'){
+                    $filteredBooks[] = $book;
+                }
+
         
-           <?php 
-           $greeting = "Hello";
-            echo $greeting . " " ."Everybody1";                     
-            ?>
-                                
+        }
+
+        ?>
+
+    <ul>
+        <?php foreach (filterByAuthor($books, 'Andy Weir' )as $book) :?>
+        <?php if ($book['author'] === 'Andy Weir') :?>
+        <li>
+            <a href = "<?= $book['purchaseUrl'] ?>">
+                <?= $book['name'];?> (<?= $book['releaseYear']?>) -By <?=$book ['author'] ?>
+            </a>
+        </li>
+        <?php endif ; ?> 
+        <?php endforeach ; ?>
+
+        <p>
+           <?= filterByAuthor($book) ?> 
+        </p>    
+
     </body>
 </html>
